0201c977fefa50c766c51ad82a4148a0aee5c5e0
Archit Kakkar <education.archit@gmail.com>
2024-05-30 01:36:54 +0530

    Why choose us section ready.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { FeatureSection } from "@/components/FeatureSection";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/95 antialiased bg-grid-white/5 text-white">
       <HeroSection />
       <FeatureSection />
+      <WhyChooseUs />
     </main>
   );
 }
