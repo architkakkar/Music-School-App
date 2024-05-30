e954123e0c269bdb71385fdbf015d697bf814519
Archit Kakkar <education.archit@gmail.com>
2024-05-31 02:37:19 +0530

    UpcomingWebinar section completed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import { FeatureSection } from "@/components/FeatureSection";
+import FeatureSection from "@/components/FeatureSection";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/95 antialiased bg-grid-white/5 text-white">
       <HeroSection />
-      {/* <FeatureSection /> */}
-      {/* <WhyChooseUs /> */}
-      {/* <MusicSchoolTestimonials /> */}
+      <FeatureSection />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
     </main>
   );
 }
