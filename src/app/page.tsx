5fb29447440ff0815337ff7959fc62ba2cb9d8fd
Archit Kakkar <education.archit@gmail.com>
2024-06-01 00:14:01 +0530

    Instructor section completed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import FeatureSection from "@/components/FeatureSection";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/95 antialiased bg-grid-white/5 text-white">
+    <main className="min-h-screen bg-black/95 antialiased text-white">
       <HeroSection />
       <FeatureSection />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
       <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
