734f88b9994bf6900b4f94e0658139ac64fea258
Archit Kakkar <education.archit@gmail.com>
2024-06-01 00:16:44 +0530

    Moved bg-grid background from page.tsx to heroSection file.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/Moving-Border";
 
 export default function HeroSection() {
   return (
-    <div className="h-screen md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-screen md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0 bg-grid-white/5 bg-black">
       <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="blue" />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
